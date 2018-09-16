d38a2cd8b8ccfa30d785cf668e8a24a5f4bd7f7a
Mick Neupart <min@nine.dk>
2018-09-16 13:26:38 +0200

    added bootstrap-vue

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import Vue from "vue";
+import BootstrapVue from "bootstrap-vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+import "@/assets/style.scss";
+
+
 Vue.config.productionTip = false;
+
+Vue.use(BootstrapVue);
 
 new Vue({
   router,
